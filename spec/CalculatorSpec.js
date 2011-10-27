1f054043d0e4e82a870ec98901241c117c09d1b8
Snorre Brandstadmoen <snorre.brandstadmoen@bekk.no>
2011-10-27 23:21:16 +0200

    Rammeverk på plass

---
+++ spec/CalculatorSpec.js
@@ -0,0 +1,14 @@
+describe("Calculator", function() {
+  var calculator;
+
+  beforeEach(function() {
+    calculator = new Calculator();
+  });
+
+  it("should add numbers", function() {
+    var sum = calculator.add(1, 2);
+	console.log(sum);
+    expect(sum).toEqual(3);
+  });
+
+});
---
+++ src/Calculator.js
@@ -0,0 +1,13 @@
+function Calculator() {
+
+	this.add = function(num1, num2) {
+		return num1 + num2;
+		
+	};
+	
+	/*
+	this.execute = function(num1, num2, functionToExecute) {
+	
+	};
+	*/
+}
