7b703a89d6e3cf41c9906ab38329fcef5bb9a5a3
Snorre Brandstadmoen <snorre.brandstadmoen@bekk.no>
2011-12-07 01:45:23 +0100

    la på fargelegging av tekst

--- spec/client/game_controller.spec.js
+++ spec/client/game_controller.spec.js
@@ -8,7 +8,7 @@ describe("GameController",
 function() {
 
     beforeEach(function() {
-        
+
         this.textToBeTypedElement = $("<div/>", {
             id: "text-to-be-typed",
         }).appendTo(document);
@@ -17,9 +17,9 @@ function() {
             id: "typed-text",
             contenteditable: "true"
         }).appendTo(document);
-         
+
         this.wrapperElement = $("<div/>", {
-             id: "wrapper",
+            id: "wrapper",
         }).appendTo(document);
 
         this.now = {
@@ -27,29 +27,32 @@ function() {
             displayTextToBeTyped: function() {},
             receiveScores: function(clientId, score) {},
             getTextToBeTyped: function() {},
-            gameOver: function() {}
+            gameOver: function() {},
+            core: {
+                clientId: "123"
+            }
         };
 
-        TDD.gameController.create({
-            typedTextElement: this.typedTextElement,
-            textToBeTypedElement: this.textToBeTypedElement,
-			wrapperElement: this.wrapperElement,
-            now: this.now
-        }).init();
+    });
 
+    afterEach(function() {
+        $(this.textToBeTypedElement).empty().remove();
+        $(this.typedTextElement).empty().remove();
+        $(this.wrapperElement).empty().remove();
     });
 
     it("should show text to be typed on init game",
     function() {
+        var originalText = "En tekst";
         var self = this;
-		var originalText = "En tekst";
         spyOn(this.now, "getTextToBeTyped").andCallFake(function() {
             self.now.displayTextToBeTyped(originalText);
         });
+
         TDD.gameController.create({
             typedTextElement: this.typedTextElement,
             textToBeTypedElement: this.textToBeTypedElement,
-			wrapperElement: this.wrapperElement,
+            wrapperElement: this.wrapperElement,
             now: this.now
         }).init();
 
@@ -58,8 +61,14 @@ function() {
 
     it("should call to validate on server on keyup",
     function() {
-		var self = this;
-		var clientId = "123";
+        var self = this;
+        var clientId = "123";
+
+        var originalText = "En tekst";
+        spyOn(this.now, "getTextToBeTyped").andCallFake(function() {
+            self.now.displayTextToBeTyped(originalText);
+        });
+
         spyOn(this.now, "validate").andCallFake(function() {
             self.now.receiveScores(clientId, {
                 "errors": [10],
@@ -67,36 +76,78 @@ function() {
             })
         });
 
+        TDD.gameController.create({
+            typedTextElement: this.typedTextElement,
+            textToBeTypedElement: this.textToBeTypedElement,
+            wrapperElement: this.wrapperElement,
+            now: this.now
+        }).init();
+
         this.typedTextElement.trigger("keyup");
 
         expect(this.now.validate).toHaveBeenCalled();
-		expect(this.wrapperElement.find("#"+clientId).text()).toContain("95%");
+        expect(this.wrapperElement.find("#" + clientId).text()).toContain("95%");
     });
 
     it("should disable text input and show message upon GAME OVER",
     function() {
-
-        });
-
-    it("should color text upon text validation",
-    function() {
         var originalText = "Dette er en test";
-        var typedText = "Dette er En";
+        var typedText = originalText;
 
         this.textToBeTypedElement.text(originalText);
         this.typedTextElement.text(typedText);
 
-        spyOn(this.now, "receiveScores").andCallFake(function() {
-            return {
-                "errors": [10],
-                "percentage": typedText.length / originalText.length * 100
-            };
+        var self = this;
+        spyOn(this.now, "validate").andCallFake(function() {
+            self.now.gameOver();
         });
+
+        TDD.gameController.create({
+            typedTextElement: this.typedTextElement,
+            textToBeTypedElement: this.textToBeTypedElement,
+            wrapperElement: this.wrapperElement,
+            now: this.now
+        }).init();
 
         this.typedTextElement.trigger("keyup");
 
-        expect(this.textToBeTypedElement.text());
+        expect(this.now.validate).toHaveBeenCalled();
+        expect(this.typedTextElement.text()).toEqual("GAME OVER!!");
+        expect(this.typedTextElement.attr("contenteditable")).toEqual("false");
+    });
 
+    it("should color text upon text validation",
+    function() {
+        var self = this;
+        var originalText = "Dette er en test";
+        var typedText = "Dette er En";
+        var errorIndex = 9;
+
+        spyOn(this.now, "validate").andCallFake(function() {
+            self.now.receiveScores("123", {
+                "errors": [9],
+                "percentage": 95
+            })
+        });
+
+        spyOn(this.now, "getTextToBeTyped").andCallFake(function() {
+            self.now.displayTextToBeTyped(originalText);
+        });
+
+        TDD.gameController.create({
+            typedTextElement: this.typedTextElement,
+            textToBeTypedElement: this.textToBeTypedElement,
+            wrapperElement: this.wrapperElement,
+            now: this.now
+        }).init();
+
+        this.typedTextElement.text(typedText);
+        this.typedTextElement.trigger("keyup");
+
+        expect(this.now.validate).toHaveBeenCalled();
+        expect($(this.textToBeTypedElement).find(":nth-child(1)").css("color")).toEqual("rgb(0, 128, 0)");
+        expect($(this.textToBeTypedElement).find(":nth-child(" + (errorIndex + 1) + ")").css("color")).toEqual("rgb(255, 0, 0)");
+        expect($(this.textToBeTypedElement).find(":nth-child(" + (typedText.length + 1) + ")").css("color")).toEqual("rgb(0, 0, 0)");
     });
 
 });
--- src/client/game_controller.js
+++ src/client/game_controller.js
@@ -15,9 +15,9 @@ if (typeof require === "function" && typeof module !== "undefined") {
                 textToBeTypedElement: {
                     value: params.textToBeTypedElement
                 },
-				wrapperElement: {
-					value: params.wrapperElement
-				},
+                wrapperElement: {
+                    value: params.wrapperElement
+                },
                 now: {
                     value: params.now
                 }
@@ -27,40 +27,64 @@ if (typeof require === "function" && typeof module !== "undefined") {
         init: function() {
             var self = this;
 
-			this.now.receiveScores = function(clientId, score) {
-				var elem = document.getElementById(clientId);
-				if (!elem) {
-					elem = $("<div/>", {
-			            id: clientId,
-						class: "test"
-			        }).appendTo(self.wrapperElement);
-				}
-				// jstestdriver.console.log(elem.attr("id"));
-				
-			    $(elem).html("ClientId: " + clientId + " Errors: " + score.errors + " Completion:" + score.percentage + "%");
-			};
-			
-			this.now.displayTextToBeTyped = function(textToBeTyped) {
-				$(self.textToBeTypedElement).html(textToBeTyped);
-			};
-			
-			this.now.gameOver = function() {
-				console.log("gameOver");
-				$(self.typedTextElement).attr("contenteditable", false).html("GAME OVER!!");
-			};
-						
+            this.now.receiveScores = function(clientId, score) {
+                var elem = document.getElementById(clientId);
+                if (!elem) {
+                    elem = $("<div/>", {
+                        id: clientId,
+                    }).appendTo(self.wrapperElement);
+                }
+
+                $(elem).html("ClientId: " + clientId + " Errors: " + score.errors + " Completion:" + score.percentage + "%");
+
+                if (clientId === self.now.core.clientId) {
+                    self.colorTextToBeTyped(score.errors);
+                }
+            };
+
+            this.now.displayTextToBeTyped = function(textToBeTyped) {
+                self.textToBeTyped = textToBeTyped;
+                $(self.textToBeTypedElement).html(textToBeTyped).lettering();
+            };
+
+            this.now.gameOver = function() {
+                $(self.typedTextElement).attr("contenteditable", false).html("GAME OVER!!");
+            };
+
             $(this.typedTextElement).keyup(function() {
                 self.now.validate($(this).text());
             });
-            
-			$(this.typedTextElement).focus();
 
-			this.now.getTextToBeTyped();
+            $(this.typedTextElement).focus();
+            this.now.getTextToBeTyped();
         },
+
+        colorTextToBeTyped: function(errors) {
+			var typedText = $(this.typedTextElement).text();
+            for (var i = 0; i < this.textToBeTyped.length; i++) {
+                var span = $(this.textToBeTypedElement).find(":nth-child(" + (i + 1) + ")");
+
+                if ($.inArray(i, errors) > -1) {
+                    span.css({
+                        "color": "rgb(255, 0, 0)"
+                    });
+                }
+                else if (i >= typedText.length) {
+                    span.css({
+                        "color": "rgb(0, 0, 0)"
+                    });                    
+                }
+                else {
+                    span.css({
+                        "color": "rgb(0, 128, 0)"
+                    });
+                }
+            };
+        }
 
     };
 
-	
+
 } ());
 
 if (typeof module === 'object') {
