e2201c72d93d1b2fe2a561364902807e80493e38
Snorre Brandstadmoen <snb@forskningsradet.no>
2014-05-27 19:13:01 +0200

    Heroku

--- src/client/game_controller.js
+++ src/client/game_controller.js
@@ -84,7 +84,7 @@ var TDD = TDD || {};
 
             this.now.gameOver = function(name) {
                 $(self.typedTextElement).unbind();
-                $(self.typedTextElement).attr("contenteditable", false).html("Game over!! " + name + " vant");
+                $(self.typedTextElement).attr("contenteditable", false).html("<strong>GAME OVER</strong> " + name + " vant");
             };
 
             $(self.typedTextElement).attr("contenteditable", false);
