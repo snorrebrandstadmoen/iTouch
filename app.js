aa9565700c23aa985aa026bd872cd1648aed511e
Snorre Brandstadmoen <snb@forskningsradet.no>
2014-05-27 18:32:12 +0200

    Heroku

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express.createServer();
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/lib'));
 app.use(express.static(__dirname + '/src/client'));
-app.listen(8080);
+app.listen(process.env.PORT || 5000);
 
 var nowjs = require("now");
 everyone = nowjs.initialize(app);
